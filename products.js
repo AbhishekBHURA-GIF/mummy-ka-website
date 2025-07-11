8a7373a96774de69622353ef27d861246fad3d58
AbhishekBHURA-GIF <140936899+AbhishekBHURA-GIF@users.noreply.github.com>
2025-07-11 21:12:39 +0530

    Add files via upload

---
+++ products.js
@@ -0,0 +1,140 @@
+const products = [
+  {
+    id: 1,
+    name: "Mystic Metal Mor (pair) ",
+    price: 140,
+    image: "images/Mystic_Metal_Mor_pair.jpg"
+  },
+  {
+    id: 2,
+    name: "Golden Crystal Phool Rakhi (pair)",
+    price: 110,
+    image: "images/Golden_Crystal_ Phool _Rakhi.jpg"
+  },
+  {
+    id: 3,
+    name: "Mayur Charm Rakhi (pair)",
+    price: 130,
+    image: "images/Mayur_Charm_Rakhi.jpg"
+ },
+  {
+    id: 4,
+    name: "Aqua Metal Mayur Rakhi (pair)",
+    price: 130,
+    image: "images/Aqua_Metal_Mayur_Rakhi.jpg"
+  },
+  {
+    id: 5,
+    name: "Pastel Bloom Rakhi (pair)",
+    price: 110,
+    image: "images/Pastel_Bloom_Rakhi.jpg"
+  },
+  {
+    id: 6,
+    name: "Pink Cute Flower Rakhi (pair)",
+    price: 130,
+    image: "images/Pink_Cute_Flower_Rakhi.jpg"
+  },
+  {
+    id: 7,
+    name: "Red Rose Pearl Rakhii (pair)",
+    price: 60,
+    image: "images/Red_Rose_Pearl_Rakhi.jpg"
+  },
+  {
+    id: 8,
+    name: "Peacock With Nazar Amulet  (pair)",
+    price: 120,
+    image: "images/Peacock_With_Nazar_Amulet.jpg"
+  },
+  {
+    id: 9,
+    name: "Pink Shine With Pearl (pair)",
+    price: 90,
+    image: "images/Pink_Shine_With_Pearl.jpg"
+  },
+  {
+    id: 10,
+    name: "Green Shine With Pearl (pair)",
+    price: 70,
+    image: "images/Green_Shine_With_Pearl.jpg"
+  },
+  {
+    id: 11,
+    name: "Red And Yellow Circle Rakhi (pair)",
+    price: 80,
+    image: "images/Red_And_Yellow_Circle_Rakhi.jpg"
+  },
+  {
+    id: 12,
+    name: "Oval Crystal Red And Yellow Rakhi (pair)",
+    price: 100,
+    image: "images/Oval_Crystal_Red_And_Yellow_Rakhi.jpg"
+  },
+  {
+    id: 13,
+    name: "Yellow Golden Flower Rakhi (pair)",
+    price: 80,
+    image: "images/Yellow_Golden_Flower_Rakhi.jpg"
+  },
+  {
+    id: 14,
+    name: "Aqua Lucky Eye Rakhi (pair)",
+    price: 60,
+    image: "images/Aqua_Lucky_Eye_Rakhi.jpg"
+  },
+  {
+    id: 15,
+    name: "Red Square Circle Rakhi (pair)",
+    price: 90,
+    image: "images/Red_Square_Circle_Rakhi.jpg"
+  },
+  {
+    id: 16,
+    name: "Blue Rakhi (pair)",
+    price: 65,
+    image: "images/Blue_Rakhi.jpg"
+  },
+  {
+    id: 17,
+    name: "Cute Pearl Rakhi (pair)",
+    price: 50,
+    image: "images/Cute_Pearl_Rakhi.jpg"
+  },
+  {
+    id: 18,
+    name: "Red Rakhi With White Rose And Pearl (pair)",
+    price: 110,
+    image: "images/Red_Rakhi_With_White_Rose_And_Pearl.jpg"
+  },
+  {
+    id: 19,
+    name: "Multi Colour Rakhi With Rounded Pearls (pair)",
+    price: 80,
+    image: "images/Multi_Colour_Rakhi_With_Rounded_Pearls.jpg"
+  },
+  {
+    id: 20,
+    name: "Pink Rakhi With White And Red Rakhi (pair)",
+    price: 100,
+    image: "images/Pink_Rakhi_With_White_And_Red_Rakhi.jpg"
+  },
+  {
+    id: 21,
+    name: "Multi Spaced Crystal Rakhi (pair)",
+    price: 100,
+    image: "images/Multi_Spaced_Crystal_Rakhi.jpg"
+  },
+  {
+    id: 22,
+    name: "Multi Colour Elegent Rakhi (pair)",
+    price: 100,
+    image: "images/Multi_Colour_Elegent_Rakhi.jpg"
+  },
+  {
+    id: 23,
+    name: "Multi Spaced Crystal Rakhi (pair)",
+    price: 100,
+    image: "images/Multi_Spaced_Crystal_Rakhi.jpg"
+  },
+];
